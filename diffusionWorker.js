8dc5c2f3152b79985456e27e54ab067a54752faf
vperoukaneas <vperoukaneas@lighthouse.gr>
2026-06-24 13:34:39 +0300

    Initial commit

---
+++ diffusionWorker.js
@@ -0,0 +1,13 @@
+// // diffusionWorker.js
+// self.onmessage = function(e) {
+    // const { initialGrid, weights } = e.data;
+    
+    // // Εδώ τοποθετείς τον βελτιστοποιημένο κώδικα applyOptimizedDiffusion
+    // // που γράψαμε στο προηγούμενο βήμα...
+    // const result = applyOptimizedDiffusion(initialGrid, weights);
+    
+    // // Επιστροφή των αποτελεσμάτων στο κύριο νήμα
+    // self.postMessage(result);
+// };
+
+
